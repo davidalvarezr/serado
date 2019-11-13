52771f1f8deb55ea199f244c2afbf2cc1d586a5f
David Alvarez <devdav@pm.me>
2019-11-13 08:16:47 +0100

    Added lists actions

---
+++ code/src/app/ngx-store/actions/lists.actions.ts
@@ -0,0 +1,31 @@
+import { createAction, props } from '@ngrx/store';
+import {AdSort} from '../../models/Models';
+
+export const LOAD_ADS = createAction(
+  '[LISTS] LOAD ADS'
+);
+
+export const LOAD_ADS_SUCCESS = createAction(
+    '[LISTS] LOAD ADS SUCCESS'
+);
+
+export const LOAD_ADS_FAILURE = createAction(
+    '[LISTS] LOAD ADS FAILURE'
+);
+
+export const SORT_ADS = createAction(
+    '[LISTS] SORT ADS',
+    props<{sort: AdSort}>()
+);
+
+export const LOAD_INFOS = createAction(
+    '[LISTS] LOAD INFOS'
+);
+
+export const LOAD_INFOS_SUCCESS = createAction(
+    '[LISTS] LOAD INFOS SUCCESS'
+);
+
+export const LOAD_INFOS_FAILURE = createAction(
+    '[LISTS] LOAD INFOS FAILURE'
+);
