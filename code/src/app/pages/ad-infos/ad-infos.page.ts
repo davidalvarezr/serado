42fdd5e0dd453b2573ac727389130e2a45842485
David Alvarez <devdav@pm.me>
2019-11-26 15:27:10 +0100

    id of current ad is now fetched asynchronously (might fix the non-working card button to go to ad-info)

--- code/src/app/pages/ad-infos/ad-infos.page.ts
+++ code/src/app/pages/ad-infos/ad-infos.page.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 import {AdService} from '../../services/ad.service';
 import {Ad, AdComplete, AdNotComplete, LatLng} from '../../models/Models';
 import {Store} from '@ngrx/store';
 import {listsSelectors, positionSelectors} from '../../ngx-store/selectors';
 import {AppState} from '../../ngx-store/reducers';
 import {Coordinates} from '@ionic-native/geolocation';
+import {map, switchMap} from 'rxjs/operators';
 
 @Component({
     selector: 'app-ad-infos',
@@ -28,33 +29,43 @@ export class AdInfosPage implements OnInit {
     }
 
     ngOnInit() {
-        this.adId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+        this.route.paramMap.pipe(
+            map((params: ParamMap) => {
+                this.adId = +params.get('id');
+                return this.adId;
+            })
+        ).subscribe(
+            adId => {
 
-        this.loading = true;
+                this.loading = true;
 
-        // Fetch the Ad info from API
-        this.adService.getOneJob(this.adId)
-            .subscribe(
-                (ad: AdComplete) => {
-                    // console.log(ad);
-                    this.loading = false;
-                    this.ad = ad;
-                },
-                error => {
-                    console.error(error);
-                    this.loading = false;
-                }
-            );
+                // Fetch the Ad info from API
+                this.adService.getOneJob(this.adId)
+                    .subscribe(
+                        (ad: AdComplete) => {
+                            // console.log(ad);
+                            this.loading = false;
+                            this.ad = ad;
+                        },
+                        error => {
+                            console.error(error);
+                            this.loading = false;
+                        }
+                    );
 
-        // Find the position of the current ad and put it in this.adPosition
-        this.store.select<Ad[]>(listsSelectors.getAdList).subscribe(adList => {
-                const maybeAd = adList.find(ad => ad.id === this.adId);
-                this.adPosition = maybeAd ? maybeAd.coordinates : null;
-            });
+                // Find the position of the current ad and put it in this.adPosition
+                this.store.select<Ad[]>(listsSelectors.getAdList).subscribe(adList => {
+                    const maybeAd = adList.find(ad => ad.id === this.adId);
+                    this.adPosition = maybeAd ? maybeAd.coordinates : null;
+                });
 
-        this.store.select<Coordinates>(positionSelectors.getCoordinatesState).subscribe(curPos => {
-           this.currentPosition = curPos;
-        });
+                this.store.select<Coordinates>(positionSelectors.getCoordinatesState).subscribe(curPos => {
+                    this.currentPosition = curPos;
+                });
+            }
+        );
+        // this.adId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+
 
         // TODO: do the same with current position
     }
