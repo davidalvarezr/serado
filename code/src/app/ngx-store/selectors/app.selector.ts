be5371b5c9dcddd43cc7524b3de4092915b7523d
David Alvarez <devdav@pm.me>
2019-11-27 23:15:13 +0100

    Added error and info selector

--- code/src/app/ngx-store/selectors/app.selector.ts
+++ code/src/app/ngx-store/selectors/app.selector.ts
@@ -12,3 +12,38 @@ export const shoulShowSpinner = createSelector(
         return positionState.shouldShowSpinner || (adsState.shouldShowSpinner );
     }
 );
+
+export const error = createSelector(
+    getPositionState,
+    getAdsState,
+    (positionState: PositionState, adsState: AdsState) => {
+        if (adsState.error) {
+            return adsState.error;
+        }
+        if (positionState.error) {
+            return positionState.error;
+        }
+        return null;
+    }
+)
+
+
+export const info = createSelector(
+    getPositionState,
+    getAdsState,
+    (positionState: PositionState, adsState: AdsState) => {
+        if (adsState.loading) {
+            return 'Chargement des annonces en cours...';
+        }
+        if (positionState.loading) {
+            return 'Localisation en cours...';
+        }
+        if (adsState.isFindingCoordinates) {
+            return 'Recherche des coordonéées à partir des adresses...';
+        }
+        if (adsState.isAddingDistances) {
+            return 'Calcul des distances en cours...';
+        }
+        return null;
+    }
+)
